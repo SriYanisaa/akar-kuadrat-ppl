124d3f2009cc39a8201796d24de21e797fdcc0f4
Sri <sriyanisaa@gmail.com>
2023-10-03 19:09:02 +0800

    Add config files.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use App\Http\Controllers\SquareRootController;
 
 Route::get('/', [SquareRootController::class, 'index'])->name('square_root.index');
 Route::post('/calculate', [SquareRootController::class, 'calculate'])->name('square_root.calculate');
-
-// Route::get('/', [SquareRootController::class,'index']);
-// Route::post('/calculate', [SquareRootController::class,'calculate'])->name('square_root.calculate');
